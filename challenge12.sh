b285ead8d918e508ae2be2c4dd05e0f5a7a3292b
mike <mikearca9823@gmail.com>
2024-04-08 18:54:58 -0500

    Push

--- challenge12.sh
+++ challenge12.sh
@@ -3,24 +3,24 @@
 # Have the program ask how your day is and echo out a response for good or bad
 # Case statment format is a little different so please look up how this would be formated
 # https://linuxize.com/post/bash-case-statement/
-echo "how is your day going (good/bad)
+echo "how is your day going (good/bad)"
 read mood
 case $mood in
 
-  PATTERN_1)
-    echo "my day is going great"
-    $mood
+  Great | Fantastic | Wonderful)
+    echo "good to hear your day is $mood"
+
     ;;
 
-  PATTERN_2)
-    echo "my day is going terrible"
+  Terrible | Bad | Shitty)
+    echo "im sorry your day is $mood"
     ;;
 
-  PATTERN_N)
-    echo "my day is fine"
+  Fine | Okay | Meh)
+    echo "im glad your day is $mood, hope it gets better"
     ;;
 
   *)
-    echo -n "unknown"
+    echo  "unknown"
     ;;
 esac
