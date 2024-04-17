b45bf0e56a5bea722546053c7ef77f43c38eb37f
mike <mikearca9823@gmail.com>
2024-04-17 18:45:49 -0500

    Push

---
+++ challenge15.py
@@ -0,0 +1,57 @@
+# Write a multi-line comment with your name, favorite food, and dream job on 3 different lines.
+
+
+# assign 5 different data types to 5 different variables. At least one datatype must be a string.
+a = bool(True)
+b=float(3.5)
+c = "Hello World"
+d=int(7)
+
+# print the length of your string.
+
+
+# create a new variable called savvy, and assign it the string with this phrase "Learning Python is Awesome!"
+#savvy = "Learning Python is Awesome!"
+
+
+# Replace "Awesome" with "great" in the string
+#a = savvy.replace ("Awesome", "great")
+#print (a)
+# Create and assign 3 more variables called name, age and length using the multi-variable naming method.
+name, age, length = "mike", 33, 6'
+
+
+
+# Format a new string called 'miniBio' using variables in curly brackets to complete this phrase... "Hi my name is (name), I am (tall) and (so) old today."
+miniBio = f"Hi my name is {name}, I am {length}, and {age} years old"
+print miniBio
+
+
+
+
+
+
+# Create a list of at least 5 elements of mixed data types
+l1 = ["hi, 8, .5, "time", name]
+
+
+# replace a part of it with something else
+l1[0] ="bye"
+print (l1)
+# append or insert several more items to the list
+l1.append(age)
+# find and print the length of the list
+
+# slice a sub-section of the 1st list, and save it to a different 2nd list
+
+# print the 2nd list
+
+# extend your original list with the 2nd list sliced above
+
+# Create a new list called "simList" containing at least 5 elements of the same data type, either string, integer, float, or Boolean.
+
+# sort "simList", and print the list
+
+# copy the "simList" list to another 3rd list
+
+# add the 2nd and 3rd lists together into a 4th list
